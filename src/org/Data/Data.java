df284e0c5a9dfebea74030ccb98c9c572876aa93
johntyrhotes <johntyrhotes@gmail.com>
2022-09-13 12:43:43 +0530

    first commit

---
+++ src/org/Data/Data.java
@@ -0,0 +1,40 @@
+package org.Data;
+
+public class Data {
+
+	public static void main(String[] args) {
+      byte a = 35;
+      System.out.println("call a :" +a);
+      
+      short b = 4567;
+      System.out.println("call b :" +b);
+      
+      int c = 123456789;
+      System.out.println("call c :" +c);
+      
+      long d = 0111111111111765432l;
+      System.out.println("call d :" +d);
+      
+      float e = 6.2f;
+      System.out.println("call e :" +e);
+      
+      double f = 6.5555;
+      System.out.println("call f :" +f);
+      
+      char g = 'a';
+      System.out.println("call a :" +g);
+      
+      boolean h = true;
+      System.out.println("worst class :" +h);
+      
+      String city = "vriddhachalam";
+      System.out.println("Name of the city :" +city);
+      
+      float x = 20/3f;
+      System.out.println(x);
+      
+      double y = 20/3d;
+      System.out.println(y);
+	}
+
+}
